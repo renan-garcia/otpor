b2685fc107d6d9596ae88dbe26073d36a614f74b
Renan Garcia <email@renangarcia.me>
2024-08-16 10:15:06 -0300

    build(app): first commit

---
+++ spec/otpor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Otpor do
+  it "has a version number" do
+    expect(Otpor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
